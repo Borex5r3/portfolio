2d8fb64a29e12a9e71048b5c1e2f9de3a3aba455
borex <abdlahdbich@gmail.com>
2024-01-23 15:53:54 +0100

    adding messages when user doesn't respect form's format

--- src/pages/contact me/ContactMe.tsx
+++ src/pages/contact me/ContactMe.tsx
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function ContactMe() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
   const [nameValue, setNameValue] = useState("");
   const [emailValue, setEmailValue] = useState("");
   const [messageValue, setMessageValue] = useState("");
@@ -18,8 +25,16 @@ function ContactMe() {
   const handleMessageInput = (e) => {
     setMessageValue(e.target.value);
   };
+  const onSubmit = async (data) => {
+    console.log(data);
+    reset();
+    setMessageValue("");
+    setNameValue("");
+    setEmailValue("");
+  };
+
   return (
-    <div className="h-[736px] min-w-[280px] flex flex-col bg-black text-[#C8C8C8] items-center pt-[86px] pb-[46px] px-[23px] font-light space-y-[40px]">
+    <div className="h-[736px] min-w-[280px] flex flex-col bg-black text-[#C8C8C8] items-center pt-[86px] pb-[46px] px-[23px] lg:px-[15vw] font-light space-y-[50px]">
       <div className="space-y-[17px] flex flex-col w-full">
         <p className="w-full text-[20px] lg:text-[30px] text-[#7AFBB9] font-medium">
           Contact Me
@@ -29,58 +44,124 @@ function ContactMe() {
           something amazing together!
         </p>
       </div>
-      <div className="w-full flex flex-col space-y-[35px]">
-        <div className="space-y-[20px]">
-          <div className="flex flex-col space-y-[20px] lg:flex-row lg:space-y-0 lg:space-x-5">
-            <div className="group relative flex items-center lg:w-1/2">
+      <form
+        className="w-full flex flex-col space-y-[35px]"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="space-y-[30px]">
+          <div className="flex flex-col space-y-[30px] lg:flex-row lg:space-y-0 lg:space-x-5">
+            <div className="group relative flex flex-col justify-center lg:w-1/2">
               <label
                 htmlFor="Name"
-                className={`absolute text-gray-500 ml-[15px] opacity-80 transition-all duration-200 ${nameValue ? styleLabel : "group-focus-within:text-[0.7rem] group-focus-within:translate-y-[-21px] group-focus-within:bg-black group-focus-within:border-x-2 group-focus-within:opacity-100  group-focus-within:px-2"}`}
+                className={`absolute text-gray-500  ml-[15px] transition-all duration-200 ${
+                  nameValue
+                    ? styleLabel
+                    : "group-focus-within:text-[0.7rem] group-focus-within:translate-y-[-21px] group-focus-within:bg-black group-focus-within:border-x-2 group-focus-within:opacity-100  group-focus-within:px-2 opacity-80"
+                }`}
               >
                 Name
               </label>
               <input
+                {...register("name", {
+                  required: "required",
+                })}
                 onChange={handleNameInput}
                 type="text"
                 id="Name"
                 className="w-full bg-inherit border border-[#D9D9D9] rounded-[6px] h-[35px] px-[15px] outline-none py-[20px]"
               />
+              <p
+                className={
+                  errors.name
+                    ? `absolute bottom-[-28px] text-[12px] lg:text-[13px] text-[#e87c03] py-[6px] px-[3px]`
+                    : "hidden"
+                }
+              >
+                {errors.name?.message}
+              </p>
             </div>
             <div className="group relative flex items-center lg:w-1/2">
               <label
                 htmlFor="Email"
-                className={`absolute text-gray-500 ml-[15px] opacity-80 group-focus-within:text-[0.7rem] transition-all duration-200 ${emailValue ? styleLabel : "group-focus-within:translate-y-[-21px] group-focus-within:bg-black group-focus-within:border-x-2 group-focus-within:opacity-100  group-focus-within:px-2"}`}
+                className={`absolute text-gray-500 ml-[15px]  group-focus-within:text-[0.7rem] transition-all duration-200 ${
+                  emailValue
+                    ? styleLabel
+                    : "opacity-80 group-focus-within:translate-y-[-21px] group-focus-within:bg-black group-focus-within:border-x-2 group-focus-within:opacity-100  group-focus-within:px-2"
+                }`}
               >
                 Email
               </label>
               <input
+                {...register("email", {
+                  required: "required",
+                  pattern: {
+                    value: /\S+@\S+\.\S+/,
+                    message:
+                      "Please enter a valid email address in the format user@example.com.",
+                  },
+                })}
                 onChange={handleEmailInput}
                 type="email"
                 id="Email"
                 className="w-full bg-inherit border border-[#D9D9D9] rounded-[6px] h-[35px] px-[15px] outline-none py-[20px]"
               />
+              <p
+                className={
+                  errors.email
+                    ? `absolute bottom-[-28px] text-[12px] lg:text-[13px] text-[#e87c03] py-[6px] px-[3px]`
+                    : "hidden"
+                }
+              >
+                {errors.email?.message}
+              </p>
             </div>
           </div>
           <div className="relative flex group">
             <label
-              htmlFor="Message"
-              className={`absolute text-gray-500 ml-[15px] mt-[10px] opacity-80 transition-all duration-200 ${messageValue ? styleMsgLabel : "group-focus-within:text-[0.7rem] group-focus-within:translate-y-[-18px] group-focus-within:bg-black group-focus-within:border-x-2 group-focus-within:opacity-100  group-focus-within:px-2"}`}
+              htmlFor="userMessage"
+              className={`absolute text-gray-500 ml-[15px] mt-[10px]  transition-all duration-200 ${
+                messageValue
+                  ? styleMsgLabel
+                  : "opacity-80 group-focus-within:text-[0.7rem] group-focus-within:translate-y-[-18px] group-focus-within:bg-black group-focus-within:border-x-2 group-focus-within:opacity-100  group-focus-within:px-2"
+              }`}
             >
               Your Message
             </label>
             <textarea
+              {...register("userMessage", {
+                required: "required",
+                maxLength: {
+                  value: 1000,
+                  message: "Description cannot be longer than 1000 characters",
+                },
+                minLength: {
+                  value: 1,
+                  message: "Please enter your message.",
+                },
+              })}
               onChange={handleMessageInput}
-              name="Message"
-              id="Message"
+              id="userMessage"
               rows="4"
               className="w-full bg-inherit border border-[#D9D9D9] rounded-[6px] px-[15px] pt-[10px] outline-none"
-            ></textarea>
+            />
+            <p
+              className={
+                errors.userMessage
+                  ? `absolute bottom-[-28px] text-[12px] lg:text-[13px] text-[#e87c03] py-[6px] px-[3px]`
+                  : "hidden"
+              }
+            >
+              {errors.userMessage?.message}
+            </p>
           </div>
         </div>
-        <button className="border rounded-[6px] py-[5px] border-[#7AFBB9] text-[#7AFBB9] font-medium hover:text-black hover:bg-[#7AFBB9] hover:border-0 duration-200">
+        <button
+          type="submit"
+          className="border rounded-[6px] py-[5px] border-[#7AFBB9] text-[#7AFBB9] font-medium hover:text-black hover:bg-[#7AFBB9] hover:border-0 duration-200 lg:w-1/3 xl:w-1/4 2xl:w-1/5"
+        >
           Submit
         </button>
-      </div>
+      </form>
     </div>
   );
 }
