38e0c10381df5bcaff21513a7822f61816fdbebb
Borex5r3 <abdlahdbich@gmail.com>
2024-03-08 16:09:19 +0100

    changing padding of footer section

--- src/pages/footer/Footer.tsx
+++ src/pages/footer/Footer.tsx
@@ -2,8 +2,8 @@ import LinkAndGit from '../../components/LinkAndGit.tsx';
 
 function Footer() {
   return (
-    <div className="relative h-[100px] sm:h-[150px] lg:h-[200px] xl:h-[240px] 2xl:h-[400px] min-w-[280px] bg-[url('assets/footer-bg-green.jpg')] bg-cover bg-center w-full flex justify-center items-end lg:pb-[20px]">
-      <div className="absolute flex w-full px-[23px] lg:px-[15vw] lg:justify-between justify-around items-center py-[10px]">
+    <div className="relative h-[100px] sm:h-[150px] lg:h-[200px] xl:h-[240px] 2xl:h-[400px] min-w-[280px] bg-[url('assets/footer-bg-green.jpg')] bg-cover bg-center w-full flex justify-center items-end">
+      <div className="absolute flex w-full px-[23px] lg:px-[15vw] lg:justify-between justify-around items-center py-[32px]">
         <p className="text-white text-[9px] lg:text-[20px]">
           Made with Passion 💫 by <span className="text-[#7AFBB9]">Borex</span>
         </p>
